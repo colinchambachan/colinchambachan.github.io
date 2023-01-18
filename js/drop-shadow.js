8f0ab10474515f9806e4cf589c2db43946ec6077
ColinChambachan <colin.chambachan@gmail.com>
2023-01-17 21:08:48 -0500

    Update drop shadow limit

--- js/drop-shadow.js
+++ js/drop-shadow.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", (event) => {
   let scroll = this.scrollY;
   console.log(scroll);
   let navbar = document.getElementById("navbar");
-  if (scroll > 0) {
+  if (scroll > 50) {
     navbar.classList.add("shadow");
   } else {
     navbar.classList.remove("shadow");

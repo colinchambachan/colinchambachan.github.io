64fcaa683c87f2fce4b701e6cdb595b0b307bb3a
colin.chamabchan@gmail.com <colin.chambachan@gmail.com>
2024-06-17 23:10:30 -0400

    housekeeping and status update

--- js/typewriter.js
+++ js/typewriter.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 }
 
 const descriptions = [
-  "An Engineering 1 Student @ McMaster University",
+  "A Software Engineering Student @ McMaster University",
   "A Community leader",
   "A Passionate & Disciplined Learner",
 ];
